6570d460da9a5526895bc3a781e7b264ea3e67d2
PH Tools <phtools@bldgtyp.com>
2022-12-16 15:25:52 -0500

    fix(write_phpp): Handle user-warning with PHPP v9/10 Settings

--- honeybee_ph_rhino/gh_compo_io/write_PHPP.py
+++ honeybee_ph_rhino/gh_compo_io/write_PHPP.py
@@ -27,7 +27,7 @@ except ImportError as e:
 
 
 class GHCompo_WriteToPHPP(object):
-
+                
     def __init__(self, _IGH, _hb_json_file, _activate_variants, _write):
         # type: (gh_io.IGH, str, str, bool) -> None
         self.IGH = _IGH
@@ -45,7 +45,21 @@ class GHCompo_WriteToPHPP(object):
         # type: () -> None
         if self.write and self.hb_json_file:
             hb_python_site_packages = honeybee.config.folders.python_package_path
-            PHX.run.write_hbjson_to_phpp(self.hb_json_file, hb_python_site_packages, self.activate_variants)
+            stdout, stderr = PHX.run.write_hbjson_to_phpp(self.hb_json_file, hb_python_site_packages, self.activate_variants)
+            self.check_for_verification_version_warning(stdout)
         else:
             msg = "Please open a valid PHPP file in Excel, and set '_write' to True."
             self.IGH.warning(msg)
+
+    def check_for_verification_version_warning(self, _stdout):
+        # type: (str) -> None
+        for line in _stdout.split("\n"):
+            if "PHPPVersionWarning:" in line:
+                msg = (
+                    "Warning: It appears that the PHPP Version and the PHI "
+                    "Certification Version do not match? If you are writing to a "
+                    "PHPP v10, be sure to add a 'HBPH PHI-Certification' component to the "
+                    "'Building Segment', and set the '_phpp_version' to '10'."
+                    )
+                self.IGH.warning(msg)
+                return 
