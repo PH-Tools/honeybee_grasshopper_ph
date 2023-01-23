9c85f295458bedface30589bbb0d5f2314616ea0
PH Tools <phtools@bldgtyp.com>
2023-01-23 06:50:57 -0500

    format

--- honeybee_ph_rhino/gh_compo_io/prog_find_Phius_program.py
+++ honeybee_ph_rhino/gh_compo_io/prog_find_Phius_program.py
@@ -6,26 +6,25 @@
 try:
     from typing import List, Optional
 except ImportError:
-    pass #IronPython 2.7
+    pass  # IronPython 2.7
 
 try:
     from honeybee_energy.programtype import ProgramType
 except ImportError as e:
-    raise ImportError('\nFailed to import honeybee_energy:\n\t{}'.format(e))
+    raise ImportError("\nFailed to import honeybee_energy:\n\t{}".format(e))
 
 try:
     from honeybee_energy_ph.library import programtypes
 except ImportError as e:
-    raise ImportError('\nFailed to import honeybee_energy_ph:\n\t{}'.format(e))
+    raise ImportError("\nFailed to import honeybee_energy_ph:\n\t{}".format(e))
 
 try:
     from honeybee_ph_rhino import gh_io
 except ImportError as e:
-    raise ImportError('\nFailed to import honeybee_ph_rhino:\n\t{}'.format(e))
+    raise ImportError("\nFailed to import honeybee_ph_rhino:\n\t{}".format(e))
 
 
 class GHCompo_FindPhiusProgram(object):
-
     def __init__(self, _IGH, _name, _description, _protocol, _base_program):
         # type: (gh_io.IGH, str, str, str, str) -> None
         self.IGH = _IGH
@@ -42,20 +41,26 @@ class GHCompo_FindPhiusProgram(object):
             msg = "Input a name or a description of the space to find a Program."
             self.IGH.warning(msg)
             return None
-        
+
         if self.name:
-            prog_data = programtypes.load_data_from_Phius_standards(self.name, 'name', self.protocol)
+            prog_data = programtypes.load_data_from_Phius_standards(
+                self.name, "name", self.protocol
+            )
             if not prog_data:
                 msg = "No Phius data found for name: '{}'".format(self.name)
                 self.IGH.warning(msg)
         elif self.description:
-            prog_data = programtypes.load_data_from_Phius_standards(self.description, 'description', self.protocol)
+            prog_data = programtypes.load_data_from_Phius_standards(
+                self.description, "description", self.protocol
+            )
             if not prog_data:
                 msg = "No Phius data found for description: '{}'".format(self.description)
                 self.IGH.warning(msg)
         else:
             prog_data = []
-            msg = "No Phius data found for name: '{}' or description: '{}'".format(self.name, self.description)
+            msg = "No Phius data found for name: '{}' or description: '{}'".format(
+                self.name, self.description
+            )
             self.IGH.warning(msg)
 
         # ------------------------------------------------------------------------------
