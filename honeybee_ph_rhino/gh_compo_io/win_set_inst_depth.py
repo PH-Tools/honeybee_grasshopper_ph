024354ccd3b558ec858f9b43dbf39b527e15110f
PH Tools <phtools@bldgtyp.com>
2023-11-20 11:28:20 -0500

    fix(win_set_inst_depth): Fix window-depth input

--- honeybee_ph_rhino/gh_compo_io/win_set_inst_depth.py
+++ honeybee_ph_rhino/gh_compo_io/win_set_inst_depth.py
@@ -19,7 +19,6 @@ except ImportError as e:
     raise ImportError('\nFailed to import honeybee_energy_ph:\n\t{}'.format(e))
 
 try:
-    from honeybee_ph_rhino.gh_compo_io import ghio_validators
     from honeybee_ph_rhino import gh_io
 except ImportError as e:
     raise ImportError('\nFailed to import honeybee_ph_rhino:\n\t{}'.format(e))
@@ -34,25 +33,20 @@ except ImportError as e:
 class GHCompo_SetApertureInstallDepth(object):
     """Interface to collect and clean PhWindowGlazing user-inputs."""
 
-    _install_depth = ghio_validators.UnitM("_install_depth")
-
     def __init__(self, _IGH, _apertures, _install_depth):
         # type: (gh_io.IGH, List[Aperture], Optional[str]) -> None
         self.IGH = _IGH
         self._apertures = _apertures
-        self._install_depth = self.calc_install_depth(_install_depth)
+        self._install_depth = self.calc_install_depth(_install_depth or "4 in.")
 
     def calc_install_depth(self, _install_depth):
-        # type: (Optional[str]) -> float| int        
+        # type: (str) -> int | float      
         """Calculate the install depth of the window glazing, considering Rhino unit-types."""
-        
-        if not _install_depth:
-            return 0.106
 
         # -- If the user supplied an input unit, just use that
         input_value, input_unit = parse_input(_install_depth)
 
-        # -- otherwise use the Rhino document unit system
+        # -- otherwise use the Rhino document unit system as the input unit
         if not input_unit:
             input_unit = self.IGH.get_rhino_unit_system_name()
         
@@ -62,6 +56,7 @@ class GHCompo_SetApertureInstallDepth(object):
         if not install_depth:
             raise ValueError("Failed to parse install depth input {}?".format(_install_depth))
         else:
+            print("Converting: {} {} -> {:.4f} M".format(input_value, input_unit, install_depth))
             return install_depth
 
     def run(self):
