e388644dfde250a6b79d4f2bfcc545df146250c0
PH Tools <phtools@bldgtyp.com>
2024-04-10 16:17:20 -0400

    update installer

--- honeybee_ph_rhino/_component_info_.py
+++ honeybee_ph_rhino/_component_info_.py
@@ -5,7 +5,7 @@
 These are called when the component is instantiated within the Grasshopper canvas.
 """
 
-RELEASE_VERSION = "Honeybee-PH v1.3.00"
+RELEASE_VERSION = "Honeybee-PH v1.3.01"
 CATEGORY = "HB-PH"
 SUB_CATEGORIES = {
     0: "00 | Utils",
