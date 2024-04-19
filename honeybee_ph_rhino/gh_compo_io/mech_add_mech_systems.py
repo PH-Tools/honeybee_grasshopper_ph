a0a04a6ad375793ff8b444fcd88339840e66d53b
PH Tools <phtools@bldgtyp.com>
2024-04-19 10:41:53 -0400

    Update mech_add_mech_systems.py

--- honeybee_ph_rhino/gh_compo_io/mech_add_mech_systems.py
+++ honeybee_ph_rhino/gh_compo_io/mech_add_mech_systems.py
@@ -12,16 +12,15 @@ except ImportError:
 
 try:
     from honeybee import room
-    from honeybee_energy_ph.properties.hvac.idealair import IdealAirSystemPhProperties
 except ImportError as e:
     raise ImportError("\nFailed to import honeybee:\n\t{}".format(e))
 
 try:
-    from honeybee_energy_ph.hvac import heat_pumps, heating, ventilation
-    from honeybee_energy_ph.hvac.supportive_device import PhSupportiveDevice
-    from honeybee_energy_ph.properties.hvac.idealair import IdealAirSystemPhProperties
+    from honeybee_phhvac.properties.room import RoomPhHvacEquipmentProperties
+    from honeybee_phhvac import heat_pumps, heating, ventilation
+    from honeybee_phhvac.supportive_device import PhSupportiveDevice
 except ImportError as e:
-    raise ImportError("\nFailed to import honeybee_energy_ph:\n\t{}".format(e))
+    raise ImportError("\nFailed to import honeybee_phhvac:\n\t{}".format(e))
 
 
 class GHCompo_AddMechSystems(object):
@@ -39,11 +38,8 @@ class GHCompo_AddMechSystems(object):
         hb_rooms_ = []
         for hb_room in self.hb_rooms:
             # -- Build up the new HB-HVAC
-            new_hvac = copy(hb_room.properties.energy.hvac.duplicate())  # type: ignore
-
-            # -- Aliases
-            new_hvac_prop = new_hvac.properties  # type: IdealAirSystemPhProperties
-            new_hvac_prop_ph = new_hvac_prop.ph  # type: IdealAirSystemPhProperties # type: ignore
+            ph_hvac = getattr(hb_room.properties, "ph_hvac")  # type: RoomPhHvacEquipmentProperties
+            new_hvac = copy(ph_hvac.duplicate())
 
             # ---------------------------------------------------------------------------
             # -- Fresh-Air Ventilation
@@ -52,26 +48,26 @@ class GHCompo_AddMechSystems(object):
                     continue
 
                 # -- Set the new h-hvac's values to match the PH-Ventilator Inputs, if any
-                new_hvac_prop_ph.ventilation_system = self.ventilation_system
-                new_hvac.sensible_heat_recovery = self.ventilation_system.ventilation_unit.sensible_heat_recovery
-                new_hvac.latent_heat_recovery = self.ventilation_system.ventilation_unit.latent_heat_recovery
-                new_hvac.demand_controlled_ventilation = True
+                new_hvac.ventilation_system = self.ventilation_system
+                # new_hvac.sensible_heat_recovery = self.ventilation_system.ventilation_unit.sensible_heat_recovery
+                # new_hvac.latent_heat_recovery = self.ventilation_system.ventilation_unit.latent_heat_recovery
+                # new_hvac.demand_controlled_ventilation = True
 
             # ---------------------------------------------------------------------------
             # -- Space Heating / Cooling
             for ph_conditioning_system in self.conditioning_systems:
                 if isinstance(ph_conditioning_system, heat_pumps.PhHeatPumpSystem):
-                    new_hvac_prop_ph.heat_pump_systems.add(ph_conditioning_system)
+                    new_hvac.heat_pump_systems.add(ph_conditioning_system)
                 else:
-                    new_hvac_prop_ph.heating_systems.add(ph_conditioning_system)
+                    new_hvac.heating_systems.add(ph_conditioning_system)
 
             # ---------------------------------------------------------------------------
             # -- Supportive Devices
             for supportive_device in self.supportive_devices:
-                new_hvac_prop_ph.supportive_devices.add(supportive_device)
+                new_hvac.supportive_devices.add(supportive_device)
 
             new_room = hb_room.duplicate()
-            new_room.properties.energy.hvac = new_hvac  # type: ignore
+            setattr(new_room.properties, "ph_hvac", new_hvac)
             hb_rooms_.append(new_room)
 
         return hb_rooms_
