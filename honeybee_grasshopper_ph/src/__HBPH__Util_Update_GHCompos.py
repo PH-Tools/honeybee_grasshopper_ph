0484780af69775b1dff10d7a30edc3b21481482b
PH Tools <phtools@bldgtyp.com>
2024-09-19 09:06:35 +0200

    Update __HBPH__Util_Update_GHCompos.py

--- honeybee_grasshopper_ph/src/__HBPH__Util_Update_GHCompos.py
+++ honeybee_grasshopper_ph/src/__HBPH__Util_Update_GHCompos.py
@@ -1,12 +1,12 @@
 """
 Updates all the GH-Components in the Source dir (Github)
 -
-EM June 27, 2024
+EM September 19, 2024 | Antwerp, Belgium
 """
 
 ghenv.Component.Name = "__HBPH__Util_Update_GHCompos"
 ghenv.Component.NickName = "HBPH_Update_Source"
-ghenv.Component.Message = 'JUN_27_2024'
+ghenv.Component.Message = 'SEP_19_2024'
 ghenv.Component.IconDisplayMode = ghenv.Component.IconDisplayMode.application
 ghenv.Component.Category = "Honeybee-PH"
 ghenv.Component.SubCategory = "00 | Utils"
@@ -15,116 +15,124 @@ ghenv.Component.ToggleObsolete(False)
 import os
 import Grasshopper.Kernel as ghK
 import shutil
+from GhPython.Component import ZuiPythonComponent
 
-class AllComponents():
-    def __init__(self, _name_identifier, _source_dir, _ghdoc, _ghK):
+
+class NamespaceComponentsOntoCanvas():
+    """Context Manager class to add all of a namespace's GH-Components to the Canvas.
+    
+    #Usage: 
+    >>>  with NamespaceComponentsOntoCanvas("HB-REVIVE", hb_revive_source_dir, ghdoc, ghK) as compos:
+    >>>      ... ## do some things with the components
+    """
+
+    def __init__(self, _namespace, _source_dir, _ghdoc, _ghK):
         # type: (str, str, Any, Any) -> None
-        self.name_identifier = _name_identifier
+        self.namespace = _namespace
         self.source_dir = _source_dir
         self.compos = []
         self.ghdoc = _ghdoc
         self.ghK = _ghK
     
     def __enter__(self):
-        print('Adding all {} the Components to the Canvas'.format(self.name_identifier))
-        ghuserFileNames = os.listdir(self.source_dir)
+        """Add all of the namespace components to the canvas."""
+
+        print('Adding all {} the Components to the Canvas'.format(self.namespace))
+        ghuser_file_names = os.listdir(self.source_dir)
         
-        for eachCompoName in ghuserFileNames:
-            if '.' == eachCompoName[0]: continue # Fucking Mac OS....
-            if self.name_identifier not in eachCompoName: continue
-            if '__' == eachCompoName[:2]: continue
+        for compo_name in ghuser_file_names:
+            if '.' == compo_name[0]: continue # Fucking Mac OS....
             
-            print "Adding: {}".format(eachCompoName)
-            compo_address = self.source_dir + eachCompoName
+            #if self.namespace not in compo_name: continue
+            
+            if not str(compo_name).startswith(self.namespace): continue
+            if '__' == compo_name[:2]: continue
+            
+            print "Adding: {}".format(compo_name)
+            compo_address = self.source_dir + compo_name
             compo = self.ghK.GH_UserObject(compo_address).InstantiateObject()
             self.ghdoc = ghenv.Component.OnPingDocument()
             self.ghdoc.AddObject(compo, False)
             
+            # --  keep track of the components added to the canvas
             self.compos.append(compo)
             
         return self
       
     def __exit__(self, exc_type, exc_value, exc_traceback):
+        """Remove all the components added to the canvas."""
+
         print('Cleaning Up the Canvas')
         for compo in self.compos:
             self.ghdoc.RemoveObject(compo, False)
 
 
-def copy_py_code(_save_address):
-    """Copy all the PyPH ghuser python code on the Canvas to another folder"""
-    print '- '*25, 'Writing GHUser Component Python code to file', '- '*25
+def copy_py_code(_gh_component, _target_path):
+    # type: (ZuiPythonComponent, str) -> None
+    """Copy the ghuse component python code over to a save file."""
     
-    doc = ghenv.Component.OnPingDocument()
-    objs = list(doc.Objects)
+    target_path_ = os.path.join(_target_path, _gh_component.Name + '.py')
     
-    for obj in objs:    
-        if obj.Category not in ['Honeybee-PH', 'HB-PH', 'HB-PH+']:
-            continue
-        
-        if 'Code' not in dir(obj):
-            # -- Value Lists items don't have Py code
-            continue
-        
-        if 'HBPH' in obj.Name[:10]:
-            address = _save_address + obj.Name + '.py'
-            
-            if os.path.exists(address):
-                os.remove(address)
-            
-            print 'Writing {} code to: --> {}'.format(obj.Name, address)
-            with open(address, 'wb') as f:
-                f.write(obj.Code.encode('utf-8'))
+    if 'Code' not in dir(_gh_component):
+        # -- Value Lists items don't have Py code
+        return None
 
-def copy_ghuser(_source_address, _save_address):
-    """Copy the HBPH ghuser component on the Canvas over to another folder"""
-    print '- '*25, 'Copying GHUser Component Files', '- '*25
+    if os.path.exists(target_path_):
+        os.remove(target_path_)
+
+    print 'Writing {} code to: --> {}'.format(_gh_component.Name, target_path_)
+    with open(target_path_, 'wb') as f:
+        f.write(_gh_component.Code.encode('utf-8'))
+
+    return None
+
+
+def copy_ghuser(_gh_component, _source_path, _target_path):
+    # type: (ZuiPythonComponent, str, str) -> None
+    """Copy ghuser component to the save file."""
+
+    src_path_ = os.path.join(_source_path, _gh_component.Name + '.ghuser')
+    target_path_ = os.path.join(_target_path, _gh_component.Name + '.ghuser')
     
-    doc = ghenv.Component.OnPingDocument()
-    objs = list(doc.Objects)
-    
-    for obj in objs:
-        if obj.Category not in ['Honeybee-PH', 'HB-PH', 'HB-PH+']:
-            continue
+    if os.path.exists(src_path_):
+        print 'Copying: {} --> {}'.format(src_path_, target_path_)
+        shutil.copy(src_path_, target_path_)
+    else:
+        raise ValueError("Error: {} folder is missing?".format(src_path_))
 
-        if obj.Name == "__HBPH__Util_Update_GHCompos":
-            continue
-        
-        if 'HBPH' in str(obj.Name)[:10]:
-            srcAddress = _source_address + obj.Name + '.ghuser'
-            saveAddress = _save_address + obj.Name + '.ghuser'
-            
-            print 'Copying: {} --> {}'.format(srcAddress, saveAddress)
-            
-            if os.path.exists(srcAddress):
-                shutil.copy(srcAddress, saveAddress)
-
-
-def make_dir(_dir_address):
-    if not os.path.exists(_dir_address):
-        try:
-            os.mkdir(_dir_address)
-        except:
-            _dir_address = False
-            print('Please provide a valid save directory. Maybe try adding/removing the backslash to the end?')
-
+    return None
 
 
 if _run:
-    # - - - - - - HBPH Components
+    # - - - - - - HB-PH Components
     hbph_source_dir = str(r"/Users/em/Library/Application Support/McNeel/Rhinoceros/8.0/Plug-ins/Grasshopper (b45a29b1-4343-4035-989e-044e8580d9cf)/UserObjects/honeybee_grasshopper_ph/")
     hbph_save_dir_ghuser    = str(r"/Users/em/Dropbox/bldgtyp-00/00_PH_Tools/honeybee_grasshopper_ph/honeybee_grasshopper_ph/user_objects/")
     hbph_save_dir_ghuser_py = str(r"/Users/em/Dropbox/bldgtyp-00/00_PH_Tools/honeybee_grasshopper_ph/honeybee_grasshopper_ph/src/")
 
-    with AllComponents("HBPH", hbph_source_dir, ghdoc, ghK):
-        copy_ghuser(hbph_source_dir, hbph_save_dir_ghuser) #--------------- Copy over the HBPH GH-Components
-        copy_py_code(hbph_save_dir_ghuser_py) #---------------------------- Copy over all the PY Code from HBPH GH-Components
+    print '- '*25, 'Backing up HBPH Components', '- '*25
+    with NamespaceComponentsOntoCanvas("HBPH", hbph_source_dir, ghdoc, ghK) as compos:
+        for compo in compos.compos:
+            copy_ghuser(compo, hbph_source_dir, hbph_save_dir_ghuser)
+            copy_py_code(compo, hbph_save_dir_ghuser_py)
 
-
-    # - - - - - HBPH+ Components
+    # # - - - - - HB-PH+ Components
     hbph_plus_source_dir = str(r"/Users/em/Library/Application Support/McNeel/Rhinoceros/8.0/Plug-ins/Grasshopper (b45a29b1-4343-4035-989e-044e8580d9cf)/UserObjects/honeybee_grasshopper_ph_plus/")
     hbph_plus_save_dir_ghuser    = str(r"/Users/em/Dropbox/bldgtyp-00/00_PH_Tools/honeybee_grasshopper_ph_plus/honeybee_grasshopper_ph_plus/user_objects/")
     hbph_plus_save_dir_ghuser_py = str(r"/Users/em/Dropbox/bldgtyp-00/00_PH_Tools/honeybee_grasshopper_ph_plus/honeybee_grasshopper_ph_plus/src/")
     
-    with AllComponents("HBPH", hbph_plus_source_dir, ghdoc, ghK):
-        copy_ghuser(hbph_plus_source_dir, hbph_plus_save_dir_ghuser) #----- Copy over the HBPH+ GH-Components
-        copy_py_code(hbph_plus_save_dir_ghuser_py) #---------------------------- Copy over all the PY Code from HBPH+ GH-Components
+    print '- '*25, 'Backing up HBPH+ Components', '- '*25
+    with NamespaceComponentsOntoCanvas("HBPH+", hbph_plus_source_dir, ghdoc, ghK) as compos:
+        for compo in compos.compos:
+            copy_ghuser(compo, hbph_plus_source_dir, hbph_plus_save_dir_ghuser)
+            copy_py_code(compo, hbph_plus_save_dir_ghuser_py)
+
+    # - - - - - HB-REVIVE Components
+    hb_revive_source_dir = str(r"/Users/em/Library/Application Support/McNeel/Rhinoceros/8.0/Plug-ins/Grasshopper (b45a29b1-4343-4035-989e-044e8580d9cf)/UserObjects/honeybee_grasshopper_revive/")
+    hb_revive_save_dir_ghuser    = str(r"/Users/em/Dropbox/bldgtyp-00/00_PH_Tools/honeybee_grasshopper_REVIVE/honeybee_revive_grasshopper/user_objects/")
+    hb_revive_save_dir_ghuser_py = str(r"/Users/em/Dropbox/bldgtyp-00/00_PH_Tools/honeybee_grasshopper_REVIVE/honeybee_revive_grasshopper/src/")
+    
+    print '- '*25, 'Backing up HB-REVIVE Components', '- '*25
+    with NamespaceComponentsOntoCanvas("HB-REVIVE", hb_revive_source_dir, ghdoc, ghK) as compos:
+        for compo in compos.compos:
+            copy_ghuser(compo, hb_revive_source_dir, hb_revive_save_dir_ghuser)
+            copy_py_code(compo, hb_revive_save_dir_ghuser_py)
